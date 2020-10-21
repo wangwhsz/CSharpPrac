2b04bb122af068dac1cb51e739f061382fee7a13
WangzyaaaA <482673302@qq.com>
2020-10-21 19:40:46 +0800

    1

---
+++ CSharpPrac/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPrac
+{
+    class Person
+    {
+        private string cName;
+        private int nAge;
+        private char cGender;
+
+
+        public int Age
+        {
+            get
+            {
+                return nAge;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    nAge = value;
+                }
+            }
+        }
+
+        public void Say()
+        {
+
+        }
+    }
+}
---
+++ CSharpPrac/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Per = new Person();
+            Per.Age = 18;
+            Console.ReadLine();
+        }
+    }
+}
